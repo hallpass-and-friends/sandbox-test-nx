1af5aa3ba9c0293dedd2c50abd4dedae4c402562
rbanning <rob@myhallpass.com>
2022-12-06 17:56:07 -0800

    initial

---
+++ libs/shared/react-test-ui/src/lib/text.stories.tsx
@@ -0,0 +1,15 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Text } from './text';
+
+const Story: ComponentMeta<typeof Text> = {
+  component: Text,
+  title: 'Text',
+};
+export default Story;
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Gabby',
+};
